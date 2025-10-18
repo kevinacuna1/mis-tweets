51c71a1e521bcad2f2ed868c3f56b42ad09424c5
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-10-18 18:09:03 -0300

    Validando el formulario

--- js/app.js
+++ js/app.js
@@ -14,5 +14,30 @@ function eventListeners() {
 function agregarTweet(e) {
     e.preventDefault();
 
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validación
+    if (tweet === '') {
+        mostrarError('Un tweet no puede ir vacío');
+        return; // Evita que se ejecuten más líneas de código
+    }
+    
     console.log('Agregando tweet...');
+}
+
+// Muestra un mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertar el mensaje de error en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Eliminar el mensaje de error después de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
